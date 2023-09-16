e410a7802196175dbc44698ebc7a1e3d78256f81
jamornP <100953989+jamornP@users.noreply.github.com>
2023-09-16 18:53:44 +0700

    up server

--- pages/database.php
+++ pages/database.php
@@ -20,8 +20,9 @@ $dateObj = new Dates;
 // echo "</pre>";
 if(isset($_POST['adddatabase'])){
     $cdata = $matchObj->getBillByDate("count",$_POST['bi_date'],$_POST['u_id']);
-    $dataB = $matchObj->getBillByDate("data",$_POST['bi_date'],$_POST['u_id']);
-    if($cdata>0){
+    if($cdata>0){        
+        $dataB = $matchObj->getBillByDate("data",$_POST['bi_date'],$_POST['u_id']);
+        echo "มีบิลแล้ว<br>";
         $data['bi_date']=$_POST['bi_date'];
         $data['bi_court']=$_POST['bi_court'];
         $data['bi_game']=$_POST['bi_game'];
@@ -29,12 +30,20 @@ if(isset($_POST['adddatabase'])){
         $data['bad_count']=$_POST['bad_count'];
         $data['bad_sum']=$_POST['bad_sum'];
         $data['u_id']=$_POST['u_id'];
+        echo "1.updatebill<br>";
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
         $ckU = $matchObj->updateBill($data);
         if($ckU){
+            echo "2.delete databill<br>";
+            echo "bi_id = ".$dataB['bi_id']."<br>";
             $ckD = $matchObj->delDataBillById($dataB['bi_id']);
             if($ckD){
+                echo "3.add databill";
                 $data_bi['bi_id'] =$dataB['bi_id'];
                 $i=0;
+                $dataLine="";
                 foreach($_POST['m_name'] as $key => $m){
                     $i++;
                     $data_bi['m_name']=$_POST['m_name'][$i];
@@ -42,14 +51,18 @@ if(isset($_POST['adddatabase'])){
                     $data_bi['bad_cal']=$_POST['bad_cal'][$i];
                     $data_bi['bad_m']=$_POST['bad_m'][$i];
                     $data_bi['bi_sum']=$_POST['bi_sum'][$i];
+                    $dataLine .= $i.". ".$data_bi['m_name']." = ".$data_bi['bi_sum']." บาท"."\n";
+                    echo "<pre>";
+                    print_r($data_bi);
+                    echo "</pre>";
                     $ck = $matchObj->addDataBill($data_bi);
                 }
                 if($ck){
                     $da = datethai($data['bi_date']);
                     $p = "Link : http://161.246.23.21/badminton/pages/show1.php?bi_id={$dataB['bi_id']}";
                     $dataL ="รายละเอียด บิล วันที่ ".$da."\n";
-                    $dataL .= $p;
-                    // echo $dataL;
+                    $dataL .= $dataLine."\n".$p;
+                    echo $dataL;
                     SentLineBasic("OchohMJUJOsdFvvZJKBUuDaOrj5c6dkhvo6HticK3PA",$dataL);
                     echo "  
                         <script type='text/javascript'>
@@ -67,6 +80,7 @@ if(isset($_POST['adddatabase'])){
         }
 
     }else{
+        echo "ยังไม่มีบิล<br>";
         $data['bi_date']=$_POST['bi_date'];
         $data['bi_court']=$_POST['bi_court'];
         $data['bi_game']=$_POST['bi_game'];
@@ -74,10 +88,17 @@ if(isset($_POST['adddatabase'])){
         $data['bad_count']=$_POST['bad_count'];
         $data['bad_sum']=$_POST['bad_sum'];
         $data['u_id']=$_POST['u_id'];
+        echo "1.add bill<br>";
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
         $bi_id = $matchObj->addBill($data);
+        echo $bi_id."<br>";
         if($bi_id){
             $data_bi['bi_id'] =$bi_id;
             $i=0;
+            echo "2.add data bill";
+            $dataLine = "";
             foreach($_POST['m_name'] as $key => $m){
                 $i++;
                 $data_bi['m_name']=$_POST['m_name'][$i];
@@ -86,13 +107,17 @@ if(isset($_POST['adddatabase'])){
                 $data_bi['bad_m']=$_POST['bad_m'][$i];
                 $data_bi['bi_sum']=$_POST['bi_sum'][$i];
                 $ck = $matchObj->addDataBill($data_bi);
+                $dataLine .= $i.". ".$data_bi['m_name']." = ".$data_bi['bi_sum']." บาท"."\n";
+                echo "<pre>";
+                print_r($data_bi);
+                echo "</pre>";
             }
             if($ck){
                 $da = datethai($data['bi_date']);
                 $p = "Link : http://161.246.23.21/badminton/pages/show1.php?bi_id={$bi_id}";
                 $dataL ="รายละเอียด บิล วันที่ ".$da."\n";
-                $dataL .= $p;
-                // echo $dataL;
+                $dataL .= $dataLine."\n".$p;
+                echo $dataL;
                 SentLineBasic("OchohMJUJOsdFvvZJKBUuDaOrj5c6dkhvo6HticK3PA",$dataL);
                 echo "  
                     <script type='text/javascript'>
@@ -100,6 +125,8 @@ if(isset($_POST['adddatabase'])){
                     </script>
                 ";
             }
+        }else{
+            echo "Add bill error";
         }
     }
     
